6f20ef36a8a51631bbed4f8d23e24a1f47228285
MathLevi20 <matheuslevi545@gmail.com>
2023-06-28 11:46:05 -0300

    fix: remove unused import

--- lib/controller/MapController.dart
+++ lib/controller/MapController.dart
@@ -4,7 +4,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
-import 'ContactController.dart';
 
 class MapController {
   late LatLng center;
